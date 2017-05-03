e67cbf993b2ab9ed1c9b057b9f0c7fb51bf7fc66
Amber Won <amberywon@yahoo.com>
2017-05-03 11:48:41 -0700

    create pet activity

--- app/src/main/java/com/petpals/CreatePetActivity.java
+++ app/src/main/java/com/petpals/CreatePetActivity.java
@@ -2,6 +2,8 @@ package com.petpals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class CreatePetActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CreatePetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_pet);
+    }
+
+    public void submitName(View button) {
+        Log.d("CreatePetActivity", "submitted name");
+        String petName =
     }
 }
