7a8ae8d1a1280fd808d2a34044b37de0e6970711
Amber Won <amberywon@yahoo.com>
2017-05-29 12:19:49 -0700

    Implemented timer

--- app/src/main/java/com/petpals/MainActivity.java
+++ app/src/main/java/com/petpals/MainActivity.java
@@ -3,6 +3,8 @@ package com.petpals;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +20,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -36,8 +40,29 @@ public class MainActivity extends AppCompatActivity {
     boolean isPal;
     String petName;
     long lastFed = 0;
+    boolean justFed = false;
     int score;
     int health = 0; // Max is 10
+
+    private final static int INTERVAL = 1000 * 10; // 1000 * 60 * 60;
+    Timer timer = new Timer ();
+    TimerTask hourlyTask = new TimerTask () {
+        @Override
+        public void run () {
+            Log.d("Health", "Decrease Health");
+
+            if (health > 0) {
+                health--;
+                handler.obtainMessage(1).sendToTarget();
+            }
+        }
+    };
+
+    public Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            displayHealth();
+        }
+    };
 
     private String getPetInformation() {
         FileInputStream fileInputStream = null;
@@ -73,17 +98,14 @@ public class MainActivity extends AppCompatActivity {
         return file;
     }
 
-    private void updateHealth(boolean justFed) {
+    private void updateHealth() {
         Calendar calendar = Calendar.getInstance();
         long now = calendar.getTimeInMillis();
 
         if (justFed) {
             lastFed = now;
-            Intent intent = new Intent(this, HealthService.class);
-            intent.putExtra("LAST_FED", lastFed);
-            startService(intent);
         } else {
-            int diff = (int) ((now - lastFed)/(1000)); // in seconds for testing
+            int diff = (int) ((now - lastFed) / (1000)); // in seconds for testing
             //(int) ((now - lastFed)/(1000 * 60 * 60)); // in hours
 
             if (health - diff >= 0) {
@@ -109,44 +131,23 @@ public class MainActivity extends AppCompatActivity {
 
         String files = getPetInformation();
 
-        if (files == null){
-            isPal = false;
-            b_left.setText("receive");
-            b_middle.setText("poke");
-            b_right.setText("create");
-            b_left.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    onReceive(v);
-                }
-            });
-
-            b_middle.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    onPoke(v);
-                }
-            });
-
-            b_right.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    onPalCreate(v);
-                }
-            });
-        }
-        else {
-            Log.d("File", files);
+        Intent intent = getIntent();
+        // From CreatePetActivity
+        if (intent.getStringExtra("PET_NAME") != null || files != null) {
             isPal = true;
 
-            Intent intent = getIntent();
-            // From CreatePetActivity
             if (intent.getStringExtra("PET_NAME") != null) {
                 petName = intent.getStringExtra("PET_NAME");
                 health = 10;
-                updateHealth(true);
-            } else {
-                updateHealth(false);
+                justFed = true;
             }
 
+            Log.d("OnCreate", "Pet Name: " + petName);
+
+            updateHealth();
             displayHealth();
+            timer.scheduleAtFixedRate(hourlyTask, INTERVAL, INTERVAL);
+            Log.d("Health", "Scheduled");
 
             b_left.setText("send");
             b_middle.setText("poke");
@@ -177,6 +178,30 @@ public class MainActivity extends AppCompatActivity {
             ImageView foodImage = (ImageView) findViewById(R.id.food_view);
             foodImage.setBackgroundResource(R.drawable.food_animation);
             foodAnimation = (AnimationDrawable) foodImage.getBackground();
+        } else if (files == null){
+            isPal = false;
+            b_left.setText("receive");
+            b_middle.setText("poke");
+            b_right.setText("create");
+            b_left.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    onReceive(v);
+                }
+            });
+
+            b_middle.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    onPoke(v);
+                }
+            });
+
+            b_right.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    onPalCreate(v);
+                }
+            });
+        }
+        else {
         }
     }
 
@@ -188,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         if (isPal) {
-            updateHealth(false);
+            updateHealth();
             displayHealth();
         }
     }
@@ -214,9 +239,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Intent intent = new Intent(this, HealthService.class);
-        stopService(intent);
     }
 
     public void onSend(View v)
@@ -238,6 +260,7 @@ public class MainActivity extends AppCompatActivity {
     public void onFeed(View v)
     {
         if (health < 10) {
+            timer.cancel();
             // Toast.makeText(this, "Clicked on Feed button", Toast.LENGTH_LONG).show();
             // feed them
             if (foodAnimation.isRunning()) {
@@ -250,8 +273,13 @@ public class MainActivity extends AppCompatActivity {
             lastFed = calendar.getTimeInMillis();
 
             health++;
-            updateHealth(true);
+            justFed = true;
+            Log.d("Fed", "Health: " + health);
+
+            updateHealth();
             displayHealth();
         }
+
+        Log.d("Fed", "Health: " + health);
     }
 }
